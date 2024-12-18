d33e51bf372c38a3780256cb94c9e6d3c4ffea8a
nguyenle <lenn@falcongames.com>
2024-12-18 17:27:24 +0700

    fix: specific datasource for repository

--- message-poller/src/main/java/com/myproject/messagepoller/order/OrderOutboxMessageRepository.java
+++ message-poller/src/main/java/com/myproject/messagepoller/order/OrderOutboxMessageRepository.java
@@ -3,12 +3,14 @@ package com.myproject.messagepoller.order;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author nguyenle
  * @since 10:37 AM Wed 12/18/2024
  */
 @Repository
+@Transactional(transactionManager = "orderMessageTransactionManager")
 public interface OrderOutboxMessageRepository extends JpaRepository<OrderOutboxMessage, String> {
 
 	List<OrderOutboxMessage> findBySentIsFalse();
